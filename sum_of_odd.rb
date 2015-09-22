523a998493687528ed2feb67997445dfb4de1f11
Tony DiNitto <think41c@gmail.com>
2015-09-21 22:52:09 -0400

    Psuedo-code added for looping.

--- sum_of_odd.rb
+++ sum_of_odd.rb
@@ -3,8 +3,11 @@ class SumOfOddNumbers
 		odd_numbers
 	end
 
-	def odd_numbers
-		odd = [1,3,5,7,9]   
+	def odd_numbers(rows)
+		# Rows is always the same number of odd numbers you'll get
+		# If rows is 3 then loop through 3 times starting with one, 
+		# and adding 2 to the number. Result -> 1, 3, 5
+		# Take result and store as an array
 	end
 
 	def row_sum_odd_numbers(rows_to_sum)
