0f7cfbec176db3c89336ded71961d9a935be7c5e
Tony DiNitto <think41c@gmail.com>
2015-09-21 22:58:10 -0400

    Array of odd numbers is being established.

--- sum_of_odd.rb
+++ sum_of_odd.rb
@@ -1,6 +1,6 @@
 class SumOfOddNumbers
 	def initialize
-		odd_numbers
+		# odd_numbers
 	end
 
 	def odd_numbers(rows)
@@ -8,11 +8,18 @@ class SumOfOddNumbers
 		# If rows is 3 then loop through 3 times starting with one, 
 		# and adding 2 to the number. Result -> 1, 3, 5
 		# Take result and store as an array
+		current_number = 1
+		stored_numbers = [1]
+
+		rows.times do 
+			current_number += 2
+			stored_numbers << current_number
+		end
+		p stored_numbers
 	end
 
 	def row_sum_odd_numbers(rows_to_sum)
-		rows_to_sum = 4
-		p rows_to_sum
+		odd_numbers(rows_to_sum)
 	end
 
 end
