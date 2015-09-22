4521347fbcdaf0b39fa7cc7e914b16888b69af15
Tony DiNitto <think41c@gmail.com>
2015-09-21 23:07:18 -0400

    Sample row/pyramid shown. Result is sum of all numbers, not just the current row.

--- sum_of_odd.rb
+++ sum_of_odd.rb
@@ -15,9 +15,11 @@ class SumOfOddNumbers
 	end
 
 	def add_row(stored_numbers)
+	  final_sum = 0
 		stored_numbers.each do |x|
-			puts x
+			final_sum += x
 		end
+		puts final_sum
 	end
 
 	def row_sum_odd_numbers(rows_to_sum)
@@ -28,3 +30,8 @@ end
 
 a = SumOfOddNumbers.new
 a.row_sum_odd_numbers(4)
+#  Row sample 
+#       1          = 1
+#      3 5         = 8 
+#     7 9 11       = 27
+#   13 15 17 19    = 64
